dc19157a85b300a312b1e52e449f8a45b42167f6
chenjiaxin <chenjiaxin@guanmai.cn>
2021-06-15 17:24:22 +0800

    代码增加大量注释

--- myfst/ffst.py
+++ myfst/ffst.py
@@ -1,22 +1,24 @@
 from dataclasses import dataclass, field
 from queue import SimpleQueue
 
-hash_pool = dict()
-
 
 @dataclass
 class Node:
     key_id: int = 0
-    char: str = ''
     child: dict = field(default_factory=dict)
     final: int = 0
     encoded: int = 0
     freeze: int = 0
     edge: dict = field(default_factory=dict)
 
-    def node_hash(self):
+    def node_hash(self, k):
+        """
+
+        :param k: 该节点的 key
+        :return:
+        """
         h = "1" if self.final else "0"
-        h += self.char
+        h += k
         if self.child:
             h += ''.join(sorted(self.child.keys()))
         return h
@@ -27,9 +29,14 @@ class FST:
     root = None
     mini = []
 
+    def help_str(self, node, t=0):
+        for i, v in node.child.items():
+            print(f"{'    ' * t}{i}{getattr(v, 'key_id', '')}-{node.edge[i]}-{v.final} -->", end='\n')
+            self.help_str(v, t + 1)
+
     def __str__(self):
         if self.root.child:
-            help_str(self.root)
+            self.help_str(self.root)
         return ''
 
     def __contains__(self, item):
@@ -38,9 +45,13 @@ class FST:
 
     def __getitem__(self, item):
         ret, val = self.traverse(item)
-        return val
+        if ret and ret.final:
+            return val
+        else:
+            return KeyError(f"not find {item}")
 
     def traverse(self, item):
+        # 游标移动到字符的字符串的最后公共节点并放回
         cur = self.root
         val = 0
         for i in item:
@@ -59,73 +70,92 @@ class Builder(FST):
     root: Node = Node()
     size: int = 0
     next: int = 0
+    hash_pool: dict = field(default_factory=dict)
 
     def __setitem__(self, word, val=0):
         cur = self.root
         last_state = None
         for w in word:
+            # 如果发现是新的节点
             if w not in cur.child:
                 # 在下一次插入前，冻结上一个节点
                 if not last_state:
+                    # 如果当前还没冻结过，冻结最后一个公共节点的子节点
                     last_state = cur
                     self.replace(last_state)
-                cur.child[w] = Node(self.id, w)
+                cur.child[w] = Node(self.id)
                 cur.edge[w] = val
-                val = 0
                 self.id += 1
+                # val 已经被赋值，清零
+                val = 0
             else:
-                node = cur.child[w]
-                edge_val = cur.edge[w]
-                com = min(edge_val, val)
+                # 取得路径对应的节点和值
+                node, edge_val = cur.child[w], cur.edge[w]
+                # 取公共前缀
+                com_val = min(edge_val, val)
+                # 如果新共享节点的值小于老节点的值
                 if val < edge_val:
+                    # 老节点所有子节点值增加差值
                     for k in node.child.keys():
-                        node.edge[k] += edge_val - com
-                    cur.edge[w] = com
-                val = val - com
+                        node.edge[k] += edge_val - com_val
+                    # 老节点的值变为公共的值
+                    cur.edge[w] = com_val
+                # 剩下节点的值为 新值 - 公共值
+                val = val - com_val
             cur = cur.child[w]
+        # 遍历结束，节点增加结束状态。
         cur.final = 1
+        # fst 单词数量 + 1
         self.size += 1
 
     def replace(self, last_state: Node):
+        # 寻找公共后缀
         for k, v in last_state.child.items():
+            # 已经冻结过的节点跳过
             if v.freeze:
                 continue
+            # 递归冻结
             if v.child:
                 self.replace(v)
-            if (h := v.node_hash()) in hash_pool:
-                last_state.child[k] = hash_pool[h]
+            # 如果有相同的节点在哈希池中，替换成哈希池中的节点
+            if (h := v.node_hash(k)) in self.hash_pool:
+                last_state.child[k] = self.hash_pool[h]
+                # 节点 id 数量 -1
                 self.id -= 1
             else:
-                hash_pool[h] = v
+                # 哈希池增加新节点
+                self.hash_pool[h] = v
+            # 寻找完公共后缀后，冻结节点
             v.freeze = True
 
     def mini_list_dfs(self, node):
+        """序列化(深度优先)"""
+
         if not node.encoded:
+            # 找到当前节点最后一个子节点（利用了 Python 字典 key 是有序的特性)
             last_key = list(node.child.keys())[-1] if node.child else None
             for k, v in node.child.items():
                 last_edge = 0
+                # 遍历到最后一个子节点是，表示 last_edge = 1，方便解序列化
                 if last_key == k:
                     last_edge = 1
+                # 一个节点可以序列为 ['b', 9, 2, 0, 0]
+                # 分别表示 [路径的key，路径的值，节点的唯一id，是否为结束状态，是否为上一个节点的最后一个子节点]
                 data = [k, node.edge[k], v.key_id, v.final, last_edge]
                 self.mini.append(data)
+                # 标识当前子节点以及被序列化过，后续不重复序列化
                 node.encoded = 1
+                # 递归序列化
                 self.mini_list_dfs(v)
 
     def mini_list(self):
         self.mini_list_dfs(self.root)
         return self.mini
 
-
     def to_file(self):
         with open('mini.txt', 'w+', encoding='utf8') as f:
             for i in self.mini_list():
                 f.write(str(i) + '\n')
-
-
-def help_str(node, t=0):
-    for i, v in node.child.items():
-        print(f"{'    ' * t}{i}{getattr(v, 'key_id', '')}-{node.edge[i]}-{v.final} -->", end='\n')
-        help_str(v, t + 1)
 
 
 @dataclass
@@ -141,32 +171,41 @@ class MiniTree(FST):
     node_pool = dict()
 
     def decode(self, mini_arr):
+        # 每反序列化一个节点，就删除一个节点，这种情况下选用了 先进先出队列
         mini_queue = SimpleQueue()
-        for i in mini_arr:
-            mini_queue.put(i)
+        # 先将序列化后的节点添加到队列中
+        for node in mini_arr:
+            mini_queue.put(node)
         self.decode_help(self.root, mini_queue)
 
     def decode_help(self, node, queue):
+        # 当队列为空时，表示所有节点都反序列化了
         if queue.empty():
             return
+        # 反序列化单个节点
         key, edge_value, key_id, final, last_edge = queue.get()
+        # 如果 节点的唯一id 已经被反序列化过了，将路径指向已有的节点
         if key_id in self.node_pool:
             child_node = self.node_pool[key_id]
-            node.child[key] = child_node
-            node.edge[key] = edge_value
+            node.child[key], node.edge[key] = child_node, edge_value
         else:
+            # 如果 节点的唯一id 没有反序列化过了，创建新的节点
             child_node = MiniNode(key_id, final)
+            # 添加到已序列化的节点池中
             self.node_pool[key_id] = child_node
-            node.child[key] = child_node
-            node.edge[key] = edge_value
+            node.child[key], node.edge[key] = child_node, edge_value
+            # 如果当前节点是结束状态，结束递归反序列化
             if final:
                 return
+            # 递归 反序列化 子节点
             self.decode_help(child_node, queue)
+        # 如果是边缘节点，则递归反序列化兄弟节点
         if not last_edge:
             self.decode_help(node, queue)
 
 
 def mini_tree(mini_arr):
+    # 反序列化帮助函数
     t = MiniTree()
     t.decode(mini_arr)
     print(t)
@@ -174,18 +213,15 @@ def mini_tree(mini_arr):
 
 
 if __name__ == '__main__':
-    f = Builder()
+    builder = Builder()
     s_list = sorted(['abcd', 'bbcd', 'bfce', 'bgce', 'bgcf', "bcd"])
-    # s_list = ['bbcd', 'abcd', 'bfce', 'bgce', 'bgcf']
     key_id = [20, 10, 5, 2, 1, 7]
-    for i, v in enumerate(s_list):
-        f[v] = key_id[i]
-    print(f)
-    print(f['bfce'])
-    # print('abc' in f)
-    # print('bgcf' in f)
-    mini_list = list(f.mini_list())
+    for word, v in enumerate(s_list):
+        builder[v] = key_id[word]
+    print(builder)
+    print(builder['bfce'])
+    print('bgcf' in builder)
+    mini_list = list(builder.mini_list())
     for e, i in enumerate(mini_list):
         print(e, i)
-    # f.to_file()
     m = mini_tree(mini_list)
